5e7071c956a9d07aab98011ee41f070dc3725d50
余小波 <yuxiaobo64@gmail.com>
2019-08-19 02:50:46 +0800

    updated

--- source/go/auto.go
+++ source/go/auto.go
@@ -1,5 +1,5 @@
 package main;
- 
+
 import (
     "github.com/fsnotify/fsnotify"
     "fmt"
@@ -13,7 +13,7 @@ import (
 )
  
 type Watch struct {
-    watch *fsnotify.Watcher;
+    watch *fsnotify.Watcher
 }
 
 var (
